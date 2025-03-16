10c92d1a1e0504498fdc39d3079830b297b92457
Trinityverd <trinityverd.data@gmail.com>
2025-03-16 13:38:18 +0300

    Update settings.py

--- trinityverd/settings.py
+++ trinityverd/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-+_xtki8a16$^m-c+m_#4@fjj223id$fol_!4kemde$8**+hj63
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['trinityverdtest.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
