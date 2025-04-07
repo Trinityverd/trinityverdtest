fa28c1f5c688646719c281c0a8a2e6ec7a82deb5
Trinityverd <trinityverd.data@gmail.com>
2025-04-07 16:26:44 +0300

    Delete seed_distri directory

--- seed_distri/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SeeddistributionConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'seed_distri'
--- seed_distri/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.seed_distribution, name='seed_distri'),
-    path('seed_distributed', views.seed_distributed, name='seed_distributed'),
-    path('get-farmer-details/<int:farmer_id>/', views.get_farmer_details, name='get_farmer_details'),
-    path('export-farmers', views.export_seed, name = 'export_seed')
-]
